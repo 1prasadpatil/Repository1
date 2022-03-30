3dc2975b83676617d87213f099219f3cac761f94
1prasadpatil <prasadp.gceme@gmail.com>
2022-03-30 23:48:37 +0530

    First commit

---
+++ Demo/src/test/Addition.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int n = 1;
+		int m = 2;
+		int o = m+n;
+		System.out.println(o);
+	}
+
+}
---
+++ Demo/src/test/StringSearch.java
@@ -0,0 +1,11 @@
+package test;
+
+public class StringSearch {
+
+	public static void main(String[] args) {
+		String s1 = "selen";
+		String s2 = "ium";
+		System.out.println(s1+s2);
+	}
+
+}
