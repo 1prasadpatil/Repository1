7cd428b4164bfa7a7fc89d68968cbac5bf9ab1d8
1prasadpatil <prasadp.gceme@gmail.com>
2022-03-31 11:39:53 +0530

    new dev added

---
+++ Demo/src/develop/AppDevelop.java
@@ -0,0 +1,9 @@
+package develop;
+
+public class AppDevelop {
+
+	public static void main(String[] args) {
+		System.out.println("New application development changes are in the develop branch");
+	}
+
+}
